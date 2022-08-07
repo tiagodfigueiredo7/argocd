50a99cf821f46032350351be15009ec838d615b6
tiagodfigueired7 <tiagod.figueiredo@gmail.com>
2022-08-06 22:04:25 -0300

    cursoargocd22:04

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 		
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<h1>Hello World</h1>"))
+		w.Write([]byte("<h1>Laboratorio Tiago Figueiredo !! Hello World</h1>"))
 
 	})
 
